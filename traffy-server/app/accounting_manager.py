4ef76efb550e29697c97f9858021f64eace267e9
Niklas Merkelt <niklasmerkelt@mail.de>
2025-10-03 17:07:04 +0200

    server: Fix wrong unit

--- traffy-server/app/accounting_manager.py
+++ traffy-server/app/accounting_manager.py
@@ -534,13 +534,13 @@ class AccountingThread(threading.Thread):
             traffic_query = session.query(Traffic).filter_by(reg_key=reg_key_query.id).order_by(Traffic.timestamp.desc()).first()
             day_range = date - traffic_query.timestamp.date()
 
-            topup_volume = self.accounting_srv.get_daily_topup_volume(gib=True)
+            topup_volume = self.accounting_srv.get_daily_topup_volume()
             if reg_key_query.daily_topup_volume is not None:
                 topup_volume = reg_key_query.daily_topup_volume
 
             credit = traffic_query.credit + (topup_volume * day_range.days)
 
-            max_volume = self.accounting_srv.get_max_saved_volume(gib=True)
+            max_volume = self.accounting_srv.get_max_saved_volume()
             if reg_key_query.max_volume is not None:
                 max_volume = reg_key_query.max_volume
 
